41ab03fd0df11365c5856cff613313f4ecdfffd1
Francy Corredor <fran_ymile@hotmail.com>
2022-05-17 23:32:24 -0500

    Estructura inicial de carpetas, Explorando React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hola Ange => Esto es Burger Queen!!!!!!!!!!!!!! </h1>
+      <h2> Primeras pruebas con React </h2>
+
+    </div>
+  );
+}
+
+export default App;
